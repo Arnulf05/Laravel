38cf1a30e05e2dccb9d79e4d83e580fabf6eb06f
Arnulf05 <aasarmiento99@misena.edu.co>
2020-11-18 18:41:40 -0500

    actualice los archivos

---
+++ database/migrations/2020_11_12_205259_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('codigo',10);
+            $table->string('nombre',100);
+            $table->unsignedBigInteger('id_marca');
+            $table->integer('precio');
+            $table->integer('cant');
+            $table->timestamps();
+
+            $table->foreign('id_marca')->references('id')->on('marcas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}

7c4230256dd01055a1cbbba548f3dc058f1d1607
Arnulf05 <aasarmiento99@misena.edu.co>
2020-12-16 15:29:15 -0500

    tablas finalizadas

---
+++ database/migrations/2020_12_16_192501_create_ventas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('valor',11, 2);
+            $table->unsignedBigInteger('id_cliente');
+            $table->unsignedBigInteger('id_producto');
+            $table->timestamps();
+
+            $table->foreign('id_cliente')->references('id')->on('clientes');
+
+            $table->foreign('id_producto')->references('id')->on('productos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
